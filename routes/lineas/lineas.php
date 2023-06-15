e634fe40842fe6daf8964ead3966ad8da5e18bc0
Ningenchaser <regearwar97@gmail.com>
2023-06-14 20:35:16 -0600

    Creacion Controlador Lineas
    Se añade lineaNueva y UpdateLinea

--- routes/lineas/lineas.php
+++ routes/lineas/lineas.php
@@ -1,1 +1,19 @@
 <?php
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'namespace' => 'App\Http\Controllers\Lineas',
+    'middleware' => 'api',
+    'prefix' => 'auth',
+], function (){
+    Route::group([
+        'middleware' => 'api',
+    ], function (){
+        Route::post('Lineas/nuevaLinea','LineasEstaciones@nuevaLinea');
+        Route::post('Lineas/updateLinea','LineasEstaciones@updateLinea');
+    });
+});
+
+
+?>
+
